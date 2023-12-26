bc42e556ff9cf41c65bde6a4a503890a4291543b
Anuj Parmar <anuj.parmar@pristyncare.com>
2023-12-26 23:03:24 +0530

    made changes to msg box design - 1

--- src/app/Components/Message.tsx
+++ src/app/Components/Message.tsx
@@ -1,13 +1,16 @@
 import React from "react"
 
 type Proptype = {
-    message : string
+    message : string,
+    owner : boolean
 }
 
-export default function Message( {message} : Proptype) {
+export default function Message( {message , owner} : Proptype) {
     return (
-        <div className="inline-block max-w-1/2 bg-white text-gray-500 p-3">
-            {message}
+    <div className={`inline-block max-w-1/2 bg-white text-gray-500 p-2 self-start
+    whitespace-pre-line rounded-md ${owner ?  "self-end" : ""}`}
+        >
+            {`${message}`}
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
 "use client"
 
 import Image from 'next/image'
-import {useState} from "react"
+import { useState } from "react"
 import Message from './Components/Message'
 export default function Home() {
-  let [msg , setmsg]  = useState<string[]>([])
-  let [currMsg , setCurrMsg] = useState<string>("")
+  let [msg, setmsg] = useState<string[]>([])
+  let [currMsg, setCurrMsg] = useState<string>("")
   // console.log( currMsg)
   return (
     <div className='min-h-screen h-1'>
       <h1 className='text-xl font-bold text-center'>
         This is Chat Messenger
       </h1>
-      <div className='bg-blue-600 h-3/4 w-3/4 mx-auto my-5 flex flex-col border-2 border-slate-400
+      <div className='bg-blue-200 h-3/4 w-3/4 mx-auto my-5 flex flex-col border-2 border-slate-400
       rounded-md 
       '>
         <div className='bg-slate-950 h-12 px-4 border-black border-2 rounded-md rounded-b-none'>
@@ -26,21 +26,23 @@ export default function Home() {
             '>Logout</button>
           </div>
         </div>
-        <div className='bg-blue-200 grow'>
-          { msg.map((text) => {
-            return <Message message= {text} />
-          }) }
+        <div className='bg-blue-200 grow text-sm'>
+          <div className='flex flex-col gap-3'>
+            {msg.map((text) => {
+              return <Message message={text} owner = {true}/>
+            })}
+          </div>
         </div>
         <div className="h-15 bg-white p-2 flex justify-between gap-3">
-          <textarea onChange={(e)=> {
-            setCurrMsg(e.target.value)
-            
-          }} value = {currMsg} placeholder='Type message here' className='text-sm h-9  box-border p-1 resize-none flex-1
-           focus:outline-none
+          <textarea onChange={(e) => {
+            setCurrMsg(`${e.target.value}`)
+
+          }} value={currMsg} placeholder='Type message here' className='text-sm h-9  box-border p-1 resize-none flex-1
+           focus:outline-none 
           ' />
-          <button onClick = {
+          <button onClick={
             () => {
-              setmsg([...msg , currMsg ])
+              setmsg([...msg, currMsg])
               setCurrMsg("")
             }
           } className='self-center float-right py-1 px-3 bg-sky-500  text-white font-bold
